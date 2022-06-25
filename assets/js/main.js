e88498ccf5342e60b693b690f997deb145ceed3d
nguyễn Hậu <96273974+CongHauabc@users.noreply.github.com>
2022-06-25 16:13:57 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,3 +152,39 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+const header1 = document.querySelector('.header')
+console.log({header})
+const home1 = document.querySelector('.home')
+home1.style.marginTop = header1.offsetHeight + 'px'
+
+
+const items = [
+    'Thực Tập Sinh FrontEnd',
+    'Intern FrontEnd',
+    'FrontEnd Developer',
+    
+  ]
+  const app = document.getElementById('app')
+  let count = 0
+  let index = 0
+  let typingEffect = () => {
+    let text = items[index]
+    if (count < text.length) {
+      setTimeout(() => {
+        app.innerHTML += text[count]
+        count++
+        typingEffect()
+      }, Math.floor(Math.random(10) * 100))
+    } else {
+      count = 0;
+      index = (index + 1 < items.length) ? index + 1 : 0
+      setTimeout(() => {
+        app.innerHTML = ''
+        typingEffect()
+      }, 1500)
+    }
+  }
+  
+  typingEffect()
+ 
